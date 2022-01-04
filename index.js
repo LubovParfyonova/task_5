36ecdbeef53d4ac0202baae73a69ce9622685cdd
Lubov <lubaparfyonova@gmail.com>
2022-01-04 06:23:50 +0200

    Add js code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Делятся на 5
+
+
+let count = 0;
+const MAX_COUNT = 84;
+
+while (count++ < MAX_COUNT){
+    if (count % 5 === 0){
+        console.log(count);
+    }
+}
+console.log('The end of script');
